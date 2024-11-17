bbd1747d89cb7ee4d60ae154d0721d1de9e65ad1
34anguyen <92149408+34anguyen@users.noreply.github.com>
2024-11-18 01:10:35 +0700

    test deploy

--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -4,6 +4,10 @@ module.exports = (app) => {
 
     const version = "/api/v1";
 
+    app.get("/", (req, res) => {
+        res.send("OK");
+    })
+
     app.use(version + "/songs", songRoutes);
 
 }
